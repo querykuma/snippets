474205bea62175d4055dc60ac97088988e5d0709
Query Kuma <querykuma@gmail.com>
2019-06-23 10:35:46 +0900

    add ショートカットキー→←

--- qiita/hideArticlesNoLike.js
+++ qiita/hideArticlesNoLike.js
@@ -1,7 +1,21 @@
 //QiitaがXHR化したSingleページ(tag一覧)でLike数が0の記事を非表示にする。ついでにトレンドも非表示。
 (function hideArticlesNoLike() {
  var once = true;
+ var debug_cnt = 0;
+ var init = () => {
+  if (typeof keydown !== 'undefined') document.removeEventListener('keydown', keydown);
+  var keydown = e => {
+   if (e.key === 'ArrowRight') {
+    document.querySelector(".st-Pager_next").children[0].click();
+   } else if (e.key === 'ArrowLeft') {
+    document.querySelector(".st-Pager_prev").children[0].click();
+   }
+  };
+  document.addEventListener('keydown', keydown);
+  delay();
+ }
  var delay = () => {
+  console.log(debug_cnt++);
   var f = a0 => {
    once = true;
    document.querySelector(".p-tagShow_mainMiddle").style.display = "none";//トレンドを非表示
@@ -11,12 +25,11 @@
   };
   [...document.querySelectorAll(".tsf-ArticleList_view .tsf-Article")].map(f);
  };
- delay();
+ init();
 
  var target = document.querySelector(".tsf-ArticleList_view");//次へを押したときMutationObserverが動く
  if (typeof observer !== "undefined") observer.disconnect();
  var observer = new MutationObserver(records => {
-  console.log(records.length);
   if (once) {
    once = false;
    setTimeout(delay, 300);
