3233ad6b0916f608f584681843598006751604ce
Query Kuma <querykuma@gmail.com>
2019-06-24 01:29:19 +0900

    refactor regainConsoleLogFromTwitter

--- twitter/regainConsoleLog.js
+++ twitter/regainConsoleLog.js
@@ -1,7 +1,11 @@
-//console.logを取り戻す
-iframe=document.getElementsByTagName("iframe")[0];
-if(!iframe){
- iframe=document.createElement("iframe");
- document.body.insertAdjacentElement("beforeend",iframe);
-}
-console.log=iframe.contentWindow.console.log;
+/* console.logを取り戻す */
+(function regainConsoleLogFromTwitter() {
+ if (!/^https?:\/\/twitter\.com/.test("https://twitter.com/")) return;
+
+ iframe = document.getElementsByTagName("iframe")[0];
+ if (!iframe) {
+  iframe = document.createElement("iframe");
+  document.body.insertAdjacentElement("beforeend", iframe);
+ }
+ console.log = iframe.contentWindow.console.log;
+})();
