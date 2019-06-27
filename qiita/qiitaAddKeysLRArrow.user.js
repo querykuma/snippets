111a4595a5058eaec1ae09f3bc4cf2c3f49de553
Query Kuma <querykuma@gmail.com>
2019-06-28 08:57:29 +0900

    fix

--- qiita/qiitaAddKeysLRArrow.user.js
+++ qiita/qiitaAddKeysLRArrow.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Qiita Add LRArrow ShortcutKeys
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Qiitaのページで左右矢印キーで前後のページに移動
 // @author       Query Kuma
 // @match        https://qiita.com/*
@@ -11,7 +11,7 @@
 (function () {
  'use strict';
 
- const use_autopagerize = 0; /* autopagerizeを使っている場合は1、autopagerizeを使っていない場合は0に */
+ const use_autopagerize = 1; /* autopagerizeを使っている場合は1、autopagerizeを使っていない場合は0に */
 
  const checkTarget = e => {
   if (e.target.tagName === "INPUT") return true;
@@ -32,35 +32,60 @@
     };
    };
 
-   document.body.userscript_keys = 1;
    document.addEventListener('keydown', keydown_link);
+
+  } else if (document.querySelector('a[rel="next"][href]')) {/* ユーザー一覧 */
+   const keydown_link = e => {
+
+    if (checkTarget(e)) return;
+
+    if (e.key === 'ArrowRight') {
+     document.querySelector('a[rel="next"][href]').click();
+    } else if (e.key === 'ArrowLeft') {
+     document.querySelector('a[rel="prev"][href]').click();
+    };
+   };
+
+   document.addEventListener('keydown', keydown_link);
+
   }
  }
 
- document.activeElement.blur(); /* 検索のときフォーカスを外す */
+ document.activeElement.blur(); /* 検索結果のページでフォーカスを外す */
 
  const delayed = () => {
-  clearInterval(timer);
 
   if (!document.getElementsByClassName("st-Pager_next").length) return;
+
+  if (document.getElementsByClassName("st-Pager_next")[0].firstElementChild.getAttribute("href")) return;
 
   const keydown_pager = e => {
 
    if (checkTarget(e)) return;
 
+   const scroll_to_node = () => {
+    let scroll_node = document.querySelector(".p-tagShow_mainBottom");
+    if (!scroll_node) {
+     scroll_node = document.querySelector('[data-hyperapp-app="OrganizationNewestArticles"]'); /* organization 最新の記事 */
+    }
+    if (scroll_node) {
+     scrollTo(0, scroll_node.offsetTop);
+    }
+   }
+
    if (e.key === 'ArrowRight') {
     document.getElementsByClassName("st-Pager_next")[0].firstElementChild.click();
-    scrollTo(0, document.querySelector(".p-tagShow_mainBottom").offsetTop);
+    scroll_to_node();
    } else if (e.key === 'ArrowLeft') {
     document.getElementsByClassName("st-Pager_prev")[0].firstElementChild.click();
-    scrollTo(0, document.querySelector(".p-tagShow_mainBottom").offsetTop);
+    scroll_to_node();
    };
+
   };
 
-  document.body.userscript_keys = 1;
   document.addEventListener('keydown', keydown_pager);
  };
 
- const timer = setInterval(delayed, 1000);
+ setTimeout(delayed, 500);
 
 })();
