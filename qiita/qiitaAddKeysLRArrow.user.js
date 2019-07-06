12d532e348907c28ca71497be4db9c44b9d56c33
Query Kuma <querykuma@gmail.com>
2019-07-07 03:36:03 +0900

    feat tags and organizations

--- qiita/qiitaAddKeysLRArrow.user.js
+++ qiita/qiitaAddKeysLRArrow.user.js
@@ -1,70 +1,27 @@
 // ==UserScript==
 // @name         Qiita Add LRArrow ShortcutKeys
 // @namespace    http://tampermonkey.net/
-// @version      0.4
-// @description  Qiitaのページで左右矢印キーで前後のページに移動
+// @version      0.5
+// @description  QiitaのXHRで前後のページを取得するページにおいて左右矢印キーで前後のページに移動
 // @author       Query Kuma
-// @match        https://qiita.com/*
+// @match        https://qiita.com/tags/*
+// @match        https://qiita.com/organizations/*
 // @grant        none
 // ==/UserScript==
 
 (function () {
  'use strict';
 
- const use_autopagerize = 1; /* autopagerizeを使っている場合は1、autopagerizeを使っていない場合は0に */
-
- const checkTarget = e => {
-  if (e.target.tagName === "INPUT") return true;
-  return false;
- }
-
- if (!use_autopagerize) {
-
-  if (document.getElementsByClassName("js-next-page-link").length) {
-   const keydown_link = e => {
-
-    if (checkTarget(e)) return;
-
-    if (e.key === 'ArrowRight') {
-     document.getElementsByClassName("js-next-page-link")[0].firstElementChild.click();
-    } else if (e.key === 'ArrowLeft') {
-     document.getElementsByClassName("js-previous-page-link")[0].firstElementChild.click();
-    };
-   };
-
-   document.addEventListener('keydown', keydown_link);
-
-  } else if (document.querySelector('a[rel="next"][href]')) {/* ユーザー一覧 */
-   const keydown_link = e => {
-
-    if (checkTarget(e)) return;
-
-    if (e.key === 'ArrowRight') {
-     document.querySelector('a[rel="next"][href]').click();
-    } else if (e.key === 'ArrowLeft') {
-     document.querySelector('a[rel="prev"][href]').click();
-    };
-   };
-
-   document.addEventListener('keydown', keydown_link);
-
-  }
- }
-
- document.activeElement.blur(); /* 検索結果のページでフォーカスを外す */
-
  const delayed = () => {
 
   if (!document.getElementsByClassName("st-Pager_next").length) return;
 
-  if (document.getElementsByClassName("st-Pager_next")[0].firstElementChild.getAttribute("href")) return;
+  if (document.getElementsByClassName("st-Pager_next")[0].querySelector("a[href]")) return;
 
   const keydown_pager = e => {
 
-   if (checkTarget(e)) return;
-
    const scroll_to_node = () => {
-    let scroll_node = document.querySelector(".p-tagShow_mainBottom");
+    let scroll_node = document.querySelector(".p-tagShow_mainBottom") /* tags 最近の記事 */;
     if (!scroll_node) {
      scroll_node = document.querySelector('[data-hyperapp-app="OrganizationNewestArticles"]'); /* organization 最新の記事 */
     }
