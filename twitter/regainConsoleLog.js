89632d62cf480cedf259e897885260e97fbd44ff
Query Kuma <querykuma@gmail.com>
2019-07-22 11:26:57 +0900

    fix(twitter/regainConsole): URL, iframe.length

--- twitter/regainConsoleLog.js
+++ twitter/regainConsoleLog.js
@@ -1,9 +1,11 @@
 /* console.logを取り戻す */
 (function regainConsoleLogFromTwitter() {
- if (!/^https?:\/\/twitter\.com/.test("https://twitter.com/")) return;
+ if (!/^https?:\/\/twitter\.com/.test(document.URL)) return;
 
- iframe = document.getElementsByTagName("iframe")[0];
- if (!iframe) {
+ var iframe = document.getElementsByTagName("iframe");
+ if (iframe.length) {
+  iframe = iframe[0];
+ } else {
   iframe = document.createElement("iframe");
   document.body.insertAdjacentElement("beforeend", iframe);
  }
