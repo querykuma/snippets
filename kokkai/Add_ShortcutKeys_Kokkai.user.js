40fd9039916971d233e81295b29dc2112478c40c
Query Kuma <querykuma@users.noreply.github.com>
2020-03-20 18:00:56 +0900

    feat(kokkai/shortcut): add keyB, show_text for playbackSpeed
    GM.setValue
    refactor

--- kokkai/Add_ShortcutKeys_Kokkai.user.js
+++ kokkai/Add_ShortcutKeys_Kokkai.user.js
@@ -1,18 +1,35 @@
 // ==UserScript==
 // @name         Kokkai Add ShortcutKeys
 // @namespace    https://github.com/querykuma/
-// @version      1.02
+// @version      1.03
 // @description  国会動画(衆議院インターネット審議中継, 参議院インターネット審議中継)にショートカットキー追加
 // @author       Query Kuma
 // @match        http*://www.shugiintv.go.jp/jp/index.php*
 // @match        https://www.webtv.sangiin.go.jp/webtv/detail.php*
-// @grant        none
+// @grant        GM.getValue
+// @grant        GM.setValue
 // ==/UserScript==
+/* global GM */
 
-(function () {
+(async function () {
 	'use strict';
 
+	const moveTime = 5; //←,→
+	const moveTime2 = 10; //J,L
+	const moveTime3 = 30; //ctrlKey+←,→
+	const moveTime4 = 60; //ctrlKey+shiftKey+←,→
+	const moveVolume = 0.05; //↑,↓
+
+	const playbackSpeed1 = 1; //Q
+	const playbackSpeed2 = 1.25; //W
+	const playbackSpeed3 = 1.5; //E
+	const playbackSpeed4 = 2; //R
+
 	var video, volume;
+	var show_text_remove_setTimeout_id = 0;
+
+	//Bで戻るための1つ前のvideo.currentTime
+	var back_previous_currentTime_val = await GM.getValue("back_previous_currentTime_val") || 0;
 
 	if (document.location.hostname.search(/sangiin/) > 0) {
 
@@ -29,17 +46,6 @@
 	}
 
 	if (!video) return;
-
-	var moveTime = 5; //←,→
-	var moveTime2 = 10; //J,L
-	var moveTime3 = 30; //ctrlKey+←,→
-	var moveTime4 = 60; //ctrlKey+shiftKey+←,→
-	var moveVolume = 0.05; //↑,↓
-
-	var playbackSpeed1 = 1; //Q
-	var playbackSpeed2 = 1.25; //W
-	var playbackSpeed3 = 1.5; //E
-	var playbackSpeed4 = 2; //R
 
 	// eslint-disable-next-line complexity
 	var event_keydown = (e) => {
@@ -86,12 +92,54 @@
 		if (e.shiftKey) return;
 		if (e.altKey) return;
 
-		var change_playbackSpeed = function (playbackSpeed) {
+		const show_text_remove = function () {
+			var display_kokkai = document.getElementById('display_kokkai');
+			display_kokkai && display_kokkai.remove();
+		};
+
+		const show_text = function (text) {
+			show_text_remove();
+
+			video.parentElement.insertAdjacentHTML('beforeend', `<span id="display_kokkai">${text}</span>`);
+
+			var display_kokkai = document.getElementById('display_kokkai');
+			display_kokkai.style.zIndex = 1000;
+			display_kokkai.style.position = 'absolute';
+			display_kokkai.style.color = "#fff";
+			display_kokkai.style.textShadow = "1px 1px 0 #000, -1px -1px 0 #000, -1px 1px 0 #000, 1px -1px 0 #000, 0px 1px 0 #000,  0-1px 0 #000, -1px 0 0 #000, 1px 0 0 #000";
+			display_kokkai.style.fontSize = 'calc(10vh)';
+
+			var video_bcr = video.getBoundingClientRect();
+			var display_kokkai_bcr = display_kokkai.getBoundingClientRect();
+
+			display_kokkai.style.left = (video_bcr.width - display_kokkai_bcr.width) / 2 + 'px';
+			display_kokkai.style.top = (video_bcr.height - display_kokkai_bcr.height) / 2 + 'px';
+
+			clearTimeout(show_text_remove_setTimeout_id);
+			show_text_remove_setTimeout_id = setTimeout(show_text_remove, 500);
+		};
+
+		const change_playbackSpeed = function (playbackSpeed) {
 
 			e.preventDefault();
 
 			video.playbackRate = playbackSpeed;
 			console.log("change speed to " + video.playbackRate);
+
+			show_text('x' + video.playbackRate);
+		};
+
+		const add_back_currentTime = function () {
+
+			back_previous_currentTime_val = video.currentTime;
+			GM.setValue("back_previous_currentTime_val", back_previous_currentTime_val);
+		};
+
+		const back_previous_currentTime = function () {
+
+			var temp = video.currentTime;
+			video.currentTime = back_previous_currentTime_val;
+			back_previous_currentTime_val = temp;
 		};
 
 		switch (e.code) {
@@ -159,8 +207,10 @@
 						//ショートカットキーがDigit0からDigit9のとき
 						var digit = m[1];
 
-						e.preventDefault();
 						console.log(e.code + ": video.currentTime = " + video.currentTime);
+						add_back_currentTime();
+
+						e.preventDefault();
 						video.currentTime = video.duration / 10 * digit;
 					}
 
@@ -186,6 +236,13 @@
 							// eslint-disable-next-line no-useless-return
 							return;
 
+						case 'KeyB':
+							e.preventDefault();
+							back_previous_currentTime();
+
+							// eslint-disable-next-line no-useless-return
+							return;
+
 						default:
 							// eslint-disable-next-line no-useless-return
 							return;
@@ -196,5 +253,12 @@
 		}
 	};
 
+	const event_beforeunload = () => {
+
+		// closeしたときvideo.currentTimeを保存→openした後KeyBで戻る
+		GM.setValue("back_previous_currentTime_val", video.currentTime);
+	};
+
 	document.addEventListener('keydown', event_keydown);
+	window.addEventListener('beforeunload', event_beforeunload);
 })();
