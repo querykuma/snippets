856a1bd291409ca3ac9de2fbe0f4acc624fe4f59
Query Kuma <querykuma@users.noreply.github.com>
2020-03-26 22:50:44 +0900

    feat(qiita/LRArrow): ユーザーページ対応、organization修正

--- qiita/qiitaAddKeysLRArrow.user.js
+++ qiita/qiitaAddKeysLRArrow.user.js
@@ -1,55 +1,94 @@
 // ==UserScript==
 // @name         Qiita Add LRArrow ShortcutKeys
-// @namespace    http://tampermonkey.net/
-// @version      0.6
+// @namespace    https://github.com/querykuma/snippets/
+// @version      0.7
 // @description  QiitaのXHRで前後のページを取得するページにおいて左右矢印キーで前後のページに移動
 // @author       Query Kuma
-// @match        https://qiita.com/tags/*
-// @match        https://qiita.com/organizations/*
+// @match        https://qiita.com/*
 // @grant        none
 // ==/UserScript==
 
 (function () {
- 'use strict';
+    'use strict';
 
- const delayed = () => {
+    if (location.pathname === "/") {
 
-  if (!document.getElementsByClassName("st-Pager_next").length) return;
+        // トップページ
+        return;
+    } else {
 
-  if (document.getElementsByClassName("st-Pager_next")[0].querySelector("a[href]")) return;
+        // 頭の"/"を除いたpathname
+        var pathname2 = location.pathname.slice(1);
+        var m = pathname2.match(/([^/]+)\//);
+        if (m) {
 
-  const keydown_pager = e => {
-
-   const scroll_to_node = () => {
-    let scroll_node = document.querySelector(".p-tagShow_mainBottom") /* tags 最近の記事 */;
-    if (!scroll_node) {
-     scroll_node = document.querySelector('[data-hyperapp-app="OrganizationNewestArticles"]'); /* organization 最新の記事 */
-    }
-    if (scroll_node) {
-     scrollTo(0, scroll_node.offsetTop);
-    }
-   }
-
-   if (e.key === 'ArrowRight') {
-
-    document.getElementsByClassName("st-Pager_next")[0].firstElementChild.click();
-    scroll_to_node();
-
-   } else if (e.key === 'ArrowLeft') {
-
-    var pager_prev = document.getElementsByClassName("st-Pager_prev");
-    if (pager_prev.length) {
-     pager_prev[0].firstElementChild.click();
+            var category = m[1];
+            if (["tags", "organizations"].indexOf(category) === -1) return;
+        }
+        // 2つ目の"/"がなければユーザーページのはず
     }
 
-    scroll_to_node();
-   };
+    const delayed = () => {
 
-  };
+        if (!document.getElementsByClassName("st-Pager_next").length) return;
 
-  document.addEventListener('keydown', keydown_pager);
- };
+        if (document.getElementsByClassName("st-Pager_next")[0].querySelector("a[href]")) return;
 
- setTimeout(delayed, 800);
+        const keydown_pager = e => {
+
+            const scroll_to_node = () => {
+
+                // tags 最近の記事
+                let scroll_node = document.querySelector(".p-tagShow_mainBottom");
+
+                if (!scroll_node) {
+
+                    // organization 最新の記事
+                    scroll_node = document.querySelectorAll(".od-Content");
+                    if (scroll_node.length) {
+
+                        scroll_node = scroll_node[scroll_node.length - 1];
+                    } else {
+
+                        scroll_node = null;
+                    }
+                }
+
+                if (!scroll_node) {
+
+                    // ユーザーページ
+                    var user_class_name = [...document.getElementsByTagName('div')].map(a => a.classList[0]).filter(a => a && a.search('UserMain__Content') === 0)[0];
+
+                    scroll_node = document.getElementsByClassName(user_class_name);
+                    scroll_node = scroll_node[scroll_node.length - 1];
+                }
+
+                if (scroll_node) {
+
+                    scrollTo(0, scroll_node.offsetTop);
+                }
+            }
+
+            if (e.key === 'ArrowRight') {
+
+                document.getElementsByClassName("st-Pager_next")[0].firstElementChild.click();
+                scroll_to_node();
+
+            } else if (e.key === 'ArrowLeft') {
+
+                var pager_prev = document.getElementsByClassName("st-Pager_prev");
+                if (pager_prev.length) {
+                    pager_prev[0].firstElementChild.click();
+                }
+
+                scroll_to_node();
+            };
+
+        };
+
+        document.addEventListener('keydown', keydown_pager);
+    };
+
+    setTimeout(delayed, 800);
 
 })();
