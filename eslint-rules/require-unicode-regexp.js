bed11d45686993d9aa63ab33ddee3900b0fcb625
Query Kuma <querykuma@users.noreply.github.com>
2022-04-28 11:19:41 +0900

    feat(eslint): auto fix at require-unicode-regexp

--- eslint-rules/require-unicode-regexp.js
+++ eslint-rules/require-unicode-regexp.js
@@ -1,4 +1,8 @@
 /**
+ * Copyright (c) 2013 ESLint Team
+ * Released under the MIT license
+ * https://github.com/eslint/eslint/blob/main/LICENSE
+ *
  * @fileoverview Rule to enforce the use of `u` flag on RegExp.
  * @author Toru Nagashima
  */
@@ -35,16 +39,25 @@ module.exports = {
             requireUFlag: "Use the 'u' flag."
         },
 
+        fixable: "code",
+
         schema: []
     },
 
     create(context) {
+        const sourceCode = context.getSourceCode();
+
         return {
             "Literal[regex]"(node) {
+
+                const fix = (fixer) => {
+                    return fixer.replaceText(node, node.value + 'u');
+                };
+
                 const flags = node.regex.flags || "";
 
                 if (!flags.includes("u")) {
-                    context.report({ node, messageId: "requireUFlag" });
+                    context.report({ node, messageId: "requireUFlag", fix });
                 }
             },
 
@@ -56,11 +69,22 @@ module.exports = {
                 };
 
                 for (const { node } of tracker.iterateGlobalReferences(trackMap)) {
+
+                    const fix = (fixer) => {
+                        if (node.arguments[1]) {
+                            var text1 = sourceCode.getText(node.arguments[1]);
+                            var result = text1.replace(/(?=['"])$/u, "u");
+                            return fixer.replaceText(node.arguments[1], result);
+                        } else {
+                            return fixer.insertTextAfter(node.arguments[0], ', "u"');
+                        }
+                    };
+
                     const flagsNode = node.arguments[1];
                     const flags = getStringIfConstant(flagsNode, scope);
 
                     if (!flagsNode || (typeof flags === "string" && !flags.includes("u"))) {
-                        context.report({ node, messageId: "requireUFlag" });
+                        context.report({ node, messageId: "requireUFlag", fix });
                     }
                 }
             }
