3469782ed754f76cd7f105284722837eaa5b55c9
Query Kuma <querykuma@users.noreply.github.com>
2022-05-19 20:35:43 +0900

    feat(eslint): allow tabs in comments at no-tabs

--- eslint-rules/no-tabs.js
+++ eslint-rules/no-tabs.js
@@ -1,4 +1,9 @@
 /**
+ * Copyright (c) 2013 ESLint Team
+ * Released under the MIT license
+ * https://github.com/eslint/eslint/blob/main/LICENSE
+ * Portions Copyright (c) 2022 Query Kuma
+ *
  * @fileoverview Rule to check for tabs inside a file
  * @author Gyandeep Singh
  */
@@ -46,14 +51,42 @@ module.exports = {
         const sourceCode = context.getSourceCode();
         const allowIndentationTabs = context.options && context.options[0] && context.options[0].allowIndentationTabs;
 
+        /**
+         * Given a list of comment nodes, return the line numbers for those comments.
+         * @param {Array} comments An array of comment nodes.
+         * @returns {number[]} An array of line numbers containing comments.
+         */
+        function getCommentLineNumbers(comments) {
+            const lines = new Set();
+
+            comments.forEach(comment => {
+                const endLine = comment.type === "Block"
+                    ? comment.loc.end.line - 1
+                    : comment.loc.end.line;
+
+                for (let i = comment.loc.start.line; i <= endLine; i++) {
+                    lines.add(i);
+                }
+            });
+
+            return lines;
+        }
+
         return {
             Program(node) {
+                var comments = sourceCode.getAllComments();
+                var commentLineNumbers = getCommentLineNumbers(comments);
+
                 sourceCode.getLines().forEach((line, index) => {
                     let match;
 
                     while ((match = tabRegex.exec(line)) !== null) {
                         if (allowIndentationTabs && !anyNonWhitespaceRegex.test(line.slice(0, match.index))) {
                             continue;
+                        }
+
+                        if (commentLineNumbers.has(index + 1)) {
+                            return;
                         }
 
                         context.report({
